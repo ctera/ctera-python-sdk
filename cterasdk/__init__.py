f9dda809d6c883fbad4fa9b9d55d1011718bf3b7
Ygal Blum <ygal.blum@gmail.com>
2020-03-03 13:01:03 -0500

    Provide the SDK user access to the Enums
    Add core and edge enums to main import

--- cterasdk/__init__.py
+++ cterasdk/__init__.py
@@ -8,3 +8,5 @@ from .convert import fromjsonstr, tojsonstr, fromxmlstr, toxmlstr  # noqa: E402,
 from .exception import CTERAException  # noqa: E402, F401
 from .object import GlobalAdmin, ServicesPortal, Gateway, Agent  # noqa: E402, F401
 from .core import query  # noqa: E402, F401
+from .edge import enum as gateway_enum  # noqa: E402, F401
+from .core import enum as portal_enum  # noqa: E402, F401
