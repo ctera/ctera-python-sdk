0079373497582f8161cac1a73a13ef3e686102ad
Ygal Blum <ygal.blum@gmail.com>
2020-03-06 10:37:09 -0500

    Prepare SDK for python packaging
    setup.py update classifiers and style
    Change README file to rst format (to align with the rest of the docs)
    Makefile - Add prepare-dist and upload-test
    travis: Add deployment to pypi test environment

--- setup.py
+++ setup.py
@@ -2,40 +2,26 @@ import setuptools
 
 # url = "https://github.com/pypa/sampleproject",
 
-with open("README.md", "r") as fh:
-    
+with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    
-    name = "cterasdk",
-    
-    version = "3.1",
-    
-    author = "Saimon Michelson",
-    
-    author_email = "saimon@ctera.com",
-    
-    description = "CTERA Python SDK",
-    
-    long_description = long_description,
-    
-    long_description_content_type = "text/markdown",
-    
-    packages = setuptools.find_packages(),
-    
-    include_package_data = True,
-    
-    classifiers = [
-        
-        "Programming Language :: Python :: 3",
-        
-        "License :: GNU General Public License :: 3",
-        
+    name="cterasdk",
+    version="3.1.2",
+    author="CTERA Networks",
+    author_email="support@ctera.com",
+    description="CTERA Python SDK",
+    long_description=long_description,
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
-        
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Software Development :: Libraries :: Python Modules"
     ],
-    
-    python_requires = '>=3.5',
-    
+    python_requires='>=3.5',
 )
