07468fa14935a6e4e5b5d550be24a1ae17611cb0
Ygal Blum <ygal.blum@gmail.com>
2020-03-26 09:26:24 -0400

    Get version using setuptools_scm
    Move setup details to setup.cfg
    List excluded files and directories in Manifest.in
    remove apidoc.template

--- setup.py
+++ setup.py
@@ -1,27 +1,5 @@
 import setuptools
 
-# url = "https://github.com/pypa/sampleproject",
-
-with open("README.rst", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
-    name="cterasdk",
-    version="3.1.3",
-    author="CTERA Networks",
-    author_email="support@ctera.com",
-    description="CTERA Python SDK",
-    long_description=long_description,
-    packages=setuptools.find_packages(),
-    include_package_data=True,
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Intended Audience :: System Administrators",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3 :: Only",
-        "Topic :: Software Development :: Libraries :: Python Modules"
-    ],
-    python_requires='>=3.5',
+    use_scm_version=True,
 )
