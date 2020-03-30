480e7979d29790e8c250aab2f4d6c3794928a43e
Ygal Blum <ygal.blum@gmail.com>
2020-03-30 08:21:48 -0400

    Samples - Services Portal Create Directories

---
+++ samples/services_portal_create_directories_sample.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+from cterasdk import config, ServicesPortal, CTERAException
+
+from sample_base import CTERASDKSampleBase
+
+
+class ServicesPortalCreateDirectoriesSample(CTERASDKSampleBase):
+    _portal_address_request_string = "the Portal Address"
+    _portal_username_request_string = "the user name"
+    _directory_name_request_string = "the directory name"
+
+    def __init__(self):
+        self._services_portal = None
+
+    def run(self):
+        config.http['ssl'] = 'Trust'
+        self._connect_to_portal()
+
+        self._create_directories()
+
+        self._services_portal.logout()
+
+    def _connect_to_portal(self):
+        gateway_address = CTERASDKSampleBase._get_input(ServicesPortalCreateDirectoriesSample._portal_address_request_string)
+        self._services_portal = ServicesPortal(gateway_address)
+        username = CTERASDKSampleBase._get_input(ServicesPortalCreateDirectoriesSample._portal_username_request_string)
+        password = CTERASDKSampleBase._get_password(username)
+        self._services_portal.login(username, password)
+
+    def _create_directories(self):
+        directory_name = CTERASDKSampleBase._get_input(ServicesPortalCreateDirectoriesSample._directory_name_request_string)
+        self._services_portal.files.mkdir(directory_name, recurse=True)
+
+
+if __name__ == "__main__":
+    try:
+        ServicesPortalCreateDirectoriesSample().run()
+    except CTERAException as error:
+        print(error)
