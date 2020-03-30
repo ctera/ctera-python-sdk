9a0b3de152eb871231bc9415402efdba184241fa
Ygal Blum <ygal.blum@gmail.com>
2020-03-30 14:22:05 -0400

    Portal - Use file url when needed
    Files
    -----
    Open should use File URL
    
    Portal
    ------
    /public/publicInfo is not under api
    Remove unneeded abstract properties

--- cterasdk/core/files/open.py
+++ cterasdk/core/files/open.py
@@ -3,4 +3,4 @@ import logging
 
 def openfile(ctera_host, path):
     logging.getLogger().info('Obtaining file handle. %s', {'path': str(path.relativepath)})
-    return ctera_host.openfile(path.fullpath())
+    return ctera_host.openfile(path.fullpath(), use_file_url=True)
