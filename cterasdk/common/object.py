e7466fce011931992ad88682f12149c278573b8d
Ygal Blum <ygal.blum@gmail.com>
2020-04-06 09:16:48 -0400

    Prevent printing of password
    Object - Do not return the value of the field password in __str__
    CteraHost - Force a toString when printing object

--- cterasdk/common/object.py
+++ cterasdk/common/object.py
@@ -1,7 +1,27 @@
 # pylint: disable=too-many-instance-attributes
+import copy
 import json
 
 
 class Object:
+    _sdk_hidden = [
+        'password',
+        'awsSecretKey',
+        'sharedSecret',
+        'passPhraseSalt',
+        'encPassphrase',
+        'encryptedFolderKey',
+        'oldPassword',
+        'newPassword',
+        'secretkey',
+        'activationCode'
+    ]
+
     def __str__(self):
-        return json.dumps(self, default=lambda o: o.__dict__, indent=5)
+        def to_protected_dict(o):
+            ret = copy.deepcopy(o.__dict__)
+            for key in o._sdk_hidden:  # pylint: disable=protected-access
+                if key in ret:
+                    ret[key] = '*** The Value if Hidden by the SDK ***'
+            return ret
+        return json.dumps(self, default=to_protected_dict, indent=5)
