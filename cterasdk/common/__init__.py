9d3c9aaae4bc98cf90c1570fb722c26e2c731ef7
Saimon Michelson <saimonation@gmail.com>
2020-09-15 10:23:48 -0400

    Add space to inline comment

--- cterasdk/common/__init__.py
+++ cterasdk/common/__init__.py
@@ -1,4 +1,4 @@
 from .item import Item  # noqa: E402, F401
 from .object import Object  # noqa: E402, F401
 from .datetime_utils import DateTimeUtils  # noqa: E402, F401
-from .utils import union, parse_base_object_ref # noqa: E402, F401
+from .utils import union, parse_base_object_ref  # noqa: E402, F401
