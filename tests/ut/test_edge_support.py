e12c4c1f0cbd16f5539639f252b5ba8dcafc24e9
Ygal Blum <ygal.blum@gmail.com>
2021-02-17 11:37:41 -0500

    Use freeze_time instead of mocking datetime
    The current mock implementation did not work and tests failed
    Use freeze_time instead

--- tests/ut/test_edge_support.py
+++ tests/ut/test_edge_support.py
@@ -1,5 +1,6 @@
 import datetime
-from unittest import mock
+
+from freezegun import freeze_time
 
 from cterasdk import exception, config
 from cterasdk.edge import support
@@ -12,7 +13,6 @@ class TestEdgeSupport(base_edge.BaseEdgeTest):
         super().setUp()
         self._levels = ['cttp', 'samba', 'auth']
         self._debug_command = 'dbg level' + ' ' + ' '.join(self._levels)
-        self._current_datetime = datetime.datetime.now()
 
     def test_set_debug_level(self):
         self._init_filer()
@@ -26,12 +26,12 @@ class TestEdgeSupport(base_edge.BaseEdgeTest):
         self.assertEqual('Invalid debug level', error.exception.message)
 
     def test_get_support_report(self):
+        current_datetime = datetime.datetime.now()
         openfile_response = 'Stream'
         self._init_filer(openfile_response=openfile_response)
         mock_save_file = self.patch_call("cterasdk.lib.filesystem.FileSystem.save")
-        with mock.patch.object(datetime, 'datetime', mock.Mock(wraps=datetime.datetime)) as patched:
-            patched.now.return_value = self._current_datetime
+        with freeze_time(current_datetime):
             support.Support(self._filer).get_support_report()
             self._filer.openfile.assert_called_once_with('/supportreport')
-            filename = 'Support-' + self._current_datetime.strftime('_%Y-%m-%dT%H_%M_%S') + '.zip'
+            filename = 'Support-' + current_datetime.strftime('_%Y-%m-%dT%H_%M_%S') + '.zip'
             mock_save_file.assert_called_once_with(config.filesystem['dl'], filename, openfile_response)
