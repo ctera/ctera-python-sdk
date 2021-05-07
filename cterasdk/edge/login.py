979076a6c8c8f03a64e44a9608e13fa1abb48b48
Saimon Michelson <58434973+saimonation@users.noreply.github.com>
2021-05-07 13:17:54 -0400

    Saimon/logout if session active (#116)
    * Logout and terminate session only for active sessions

--- cterasdk/edge/login.py
+++ cterasdk/edge/login.py
@@ -22,8 +22,11 @@ class Login(BaseCommand):
             raise error
 
     def logout(self):
+        host = self._gateway.host()
+        user = self._gateway.session().user
         try:
             self._gateway.form_data('/logout', {'foo': 'bar'})
-            logging.getLogger().info("User logged out. %s", {'host': self._gateway.host()})
+            logging.getLogger().info("User logged out. %s", {'host': self._gateway.host(), 'user': user})
         except CTERAException as error:
+            logging.getLogger().error("Logout failed. %s", {'host': host, 'user': user})
             raise error
