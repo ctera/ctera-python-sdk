45414904cb0b86162037a062955fbce3b08e8d1e
Saimon Michelson <saimonation@gmail.com>
2021-08-20 11:53:04 -0400

    Revert "Add ntp commands, fix docs and log print"
    This reverts commit b6c20e4f684958699d75e44aa8e2d26dfef1f565.

--- cterasdk/edge/ntp.py
+++ cterasdk/edge/ntp.py
@@ -7,13 +7,6 @@ from .base_command import BaseCommand
 class NTP(BaseCommand):
     """ Gateway NTP configuration """
 
-    def get_configuration(self):
-        return self._gateway.get('/config/time')
-
-    @property
-    def servers(self):
-        return self._gateway.get('/config/time/NTPServer')
-
     def enable(self, servers=None):
         """
         Enable NTP
@@ -21,16 +14,22 @@ class NTP(BaseCommand):
         :param list[str] servers: List of NTP servers address
         """
         logging.getLogger().info("Enabling time synchronization with ntp servers.")
+
         self._gateway.put('/config/time/NTPMode', Mode.Enabled)
+
         logging.getLogger().info("Time synchronization enabled.")
 
         if servers:
             logging.getLogger().info("Updating time servers. %s", {'servers': servers})
+
             self._gateway.put('/config/time/NTPServer', servers)
+
             logging.getLogger().info("Time servers updated. %s", {'servers': servers})
 
     def disable(self):
         """ Disable NTP """
         logging.getLogger().info("Disabling time synchronization with ntp servers.")
+
         self._gateway.put('/config/time/NTPMode', Mode.Disabled)
+
         logging.getLogger().info("Time synchronization disabled.")
