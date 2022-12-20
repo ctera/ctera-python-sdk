9544d832b5a4d81607b806cb31b80287f89c95da
CTERA\saimon <saimonation@gmail.com>
2022-12-20 11:34:58 -0500

    pass pylint and ut

--- tests/ut/test_core_servers.py
+++ tests/ut/test_core_servers.py
@@ -30,24 +30,24 @@ class TestCoreServers(base_core.BaseCoreTest):
             self._assert_equal_objects(actual_query_params, expected_query_params)
 
     def test_get_server_background_tasks(self):
-        self._init_global_admin(get_response=TestCoreServers._create_task_object(id=self._task_id,
-                                                                          name=self._task_name,
-                                                                          startTime=self._task_start_time,
-                                                                          endTime=self._task_end_time,
-                                                                          elapsedTime=self._task_elapsed_time,
-                                                                          status=self._task_status,
-                                                                          progstring=self._task_message
-                                                                          ))
-        servers.Servers(self._global_admin).tasks.background(self._server)
+        self._init_global_admin(get_response=[TestCoreServers._create_task_object(id=self._task_id,
+                                                                                 name=self._task_name,
+                                                                                 startTime=self._task_start_time,
+                                                                                 endTime=self._task_end_time,
+                                                                                 elapsedTime=self._task_elapsed_time,
+                                                                                 status=self._task_status,
+                                                                                 progstring=self._task_message
+                                                                                 )])
+        ret = servers.Servers(self._global_admin).tasks.background(self._server)
         self._global_admin.get.assert_called_once_with(f'/servers/{self._server}/bgTasks')
-        self.assertEqual(ret.ref, self._ref)
+        self.assertEqual(ret[0].ref, self._ref)
 
     def test_get_server_scheduled_tasks(self):
-        self._init_global_admin(get_response=TestCoreServers._create_task_object(id=self._task_id,
-                                                                          name=self._task_name,
-                                                                          startTime=self._task_start_time
-                                                                          ))
-        ret = servers.Servers(self._global_admin).tasks.scheduled(self._server)
+        self._init_global_admin(get_response=[TestCoreServers._create_task_object(id=self._task_id,
+                                                                                  name=self._task_name,
+                                                                                  startTime=self._task_start_time
+                                                                                  )])
+        servers.Servers(self._global_admin).tasks.scheduled(self._server)
         self._global_admin.get.assert_called_once_with(f'/servers/{self._server}/schedTasks')
 
     @staticmethod
