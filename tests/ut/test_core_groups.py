e22668a256ce19467fe1683b9d3cd5f7d45f325c
CTERA\saimon <saimonation@gmail.com>
2023-07-24 14:02:31 -0400

    updates to ut 001

--- tests/ut/test_core_groups.py
+++ tests/ut/test_core_groups.py
@@ -25,18 +25,13 @@ class TestCoreGroups(base_core.BaseCoreTest):
             get_multi_response = self._get_group_object(name=group_account.name)
             self._init_global_admin(get_multi_response=get_multi_response)
             ret = groups.Groups(self._global_admin).get(group_account)
+            self._global_admin.get_multi.assert_called_once_with(self._get_group_url(group_account), mock.ANY)
             expected_include = ['/' + attr for attr in groups.Groups.default]
             actual_include = self._global_admin.get_multi.call_args[0][1]
             self.assertEqual(len(expected_include), len(actual_include))
             for attr in expected_include:
                 self.assertIn(attr, actual_include)
             self.assertEqual(ret.name, group_account.name)
-        self._global_admin.get_multi.assert_has_calls(
-            [
-                mock.call(f'/localGroups/{self._groupname}', mock.ANY),
-                mock.call(f'/domains/{self._domain}/adGroups/{self._groupname}', mock.ANY)
-            ]
-        )
 
     def _get_group_url(self, group):
         return f'/localGroups/{group.name}' if group.is_local else f'/domains/{account.directory}/adGroups/{group_account.name}'
@@ -72,7 +67,7 @@ class TestCoreGroups(base_core.BaseCoreTest):
 
     def test_list_domain_groups_default_attrs(self):
         with mock.patch("cterasdk.core.groups.query.iterator") as query_iterator_mock:
-            groups.Groups(self._global_admin).list_local_groups()
+            groups.Groups(self._global_admin).list_domain_groups()
             query_iterator_mock.assert_called_once_with(self._global_admin, f'/domains/{self._domain}/adGroups', mock.ANY)
             expected_query_params = base_core.BaseCoreTest._create_query_params(include=groups.Groups.default,
                                                                                 start_from=0, count_limit=50)
@@ -84,15 +79,21 @@ class TestCoreGroups(base_core.BaseCoreTest):
         self._init_global_admin(add_response=add_response)
         ret = groups.Groups(self._global_admin).add(self._groupname, self._description)
         self._global_admin.execute.assert_called_once_with('', 'addGroup', mock.ANY)
-        expected_param = self._get_group_object(name=self._groupname, description=self._description)
+        expected_param = self._get_add_group_object(name=self._groupname, description=self._description)
         actual_param = self._global_admin.execute.call_args[0][2]
         self._assert_equal_objects(actual_param, expected_param)
         self.assertEqual(ret, add_response)
 
+    def _get_add_group_object(self, **kwargs):
+        param = Object()
+        param._classname = 'AddGroupParam'  # pylint: disable=protected-access
+        param.groupData = self._get_group_object(**kwargs)
+        return param
+
     def test_delete_group(self):
-        delete_response = 'Success'
+        execute_response = 'Success'
         for group_account in [self._local_group, self._domain_group]:
-            self._init_global_admin(delete_response=delete_response)
+            self._init_global_admin(execute_response=delete_response)
             ret = groups.Groups(self._global_admin).delete(group_account)
             self.assertEqual(ret, delete_response)
         self._global_admin.execute.assert_has_calls(
