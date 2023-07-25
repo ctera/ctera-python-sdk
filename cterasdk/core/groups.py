8274edd7d0600127d8ac7e669636d3b338aedfa0
Saimon Michelson <58434973+saimonation@users.noreply.github.com>
2023-07-25 13:20:56 -0400

    smich/add ut for core local groups (#189)

--- cterasdk/core/groups.py
+++ cterasdk/core/groups.py
@@ -40,7 +40,7 @@ class Groups(BaseCommand):
         include = ['/' + attr for attr in include]
         group_object = self._portal.get_multi(baseurl, include)
         if group_object.name is None:
-            raise ObjectNotFoundException('Could not find user', baseurl, group_directory=group_account.directory, name=group_account.name)
+            raise ObjectNotFoundException('Could not find group', baseurl, group_directory=group_account.directory, name=group_account.name)
         return group_object
 
     def list_local_groups(self, include=None):
@@ -170,7 +170,7 @@ class Groups(BaseCommand):
         :param cterasdk.core.types.GroupAccount group_account: Group account
         """
         logging.getLogger().info('Deleting group. %s', {'group': str(group_account.name)})
-        baseurl = f'/users/{group_account.name}' if group_account.is_local \
+        baseurl = f'/localGroups/{group_account.name}' if group_account.is_local \
             else f'/domains/{group_account.directory}/adGroups/{group_account.name}'
         response = self._portal.execute(baseurl, 'delete', True)
         logging.getLogger().info('Group deleted. %s', {'group': str(group_account.name)})
