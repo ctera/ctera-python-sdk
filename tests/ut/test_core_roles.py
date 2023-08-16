79324eeff3da899468d7983a526d668f3461a560
Saimon Michelson <saimon.michelson@gmail.com>
2023-08-16 16:43:01 -0400

    update unittests 1

--- tests/ut/test_core_roles.py
+++ tests/ut/test_core_roles.py
@@ -28,7 +28,7 @@ class TestCoreRoles(base_core.BaseCoreTest):
         self._init_global_admin(get_response=TestCoreRoles._role_settings(self._role))
         ret = roles.Roles(self._global_admin).get(self._role)
         self._global_admin.get.assert_called_once_with(f'/rolesSettings/{self._role_settings_resource}')
-        self._assert_equal_objects(ret, TestCoreRoles._role_settings())
+        self._assert_equal_objects(ret, TestCoreRoles._role_settings(self._role))
 
     def test_modify_role_not_found(self):
         self._init_global_admin()
