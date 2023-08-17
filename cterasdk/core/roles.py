6ae4abb2ff5497cdb32e2ed334ec0a6c43cd5612
ronier83 <ronxp@msn.com>
2023-08-17 12:33:09 +0300

    rebase roles.py into kms feature

--- cterasdk/core/roles.py
+++ cterasdk/core/roles.py
@@ -20,7 +20,7 @@ class Roles(BaseCommand):
     @staticmethod
     def find(role):
         """
-        Find Role
+        Find a Role
         """
         options = {k: v for k, v in RoleResolver.__dict__.items() if not k.startswith('_')}
         return options.get(role, None)
