96537800bbc590253e68d59342ae066a4c7ffa00
Saimon Michelson <saimon.michelson@gmail.com>
2023-08-21 14:31:53 -0400

    add doc dependencies to pass build and update theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -82,7 +82,7 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #
 #html_theme = 'default'
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
