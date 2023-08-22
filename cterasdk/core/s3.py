e3579481a4de1eeee78f0971900bcd27e1cb14c3
Ron Erez <ronxp@msn.com>
2023-08-22 21:12:36 +0300

    added new methods in s3.py

--- cterasdk/core/s3.py
+++ cterasdk/core/s3.py
@@ -1,17 +1,29 @@
 import logging
 
 from .base_command import BaseCommand
-from ..common import Object, utf8_decode, union
-from . import query
 
 
 class S3Keys(BaseCommand):
+    """
+    External S3Keys Management APIs
+    :ivar cterasdk.core.s3.S3Keys : Object holding the Portal S3 User Keys APIs
+    """
 
     def get_keys(self, user):
+        """
+        Retrieve User Access Keys
+
+        :param str user: Retrieve the Access Keys for the User
+        """
         param = self._portal.users.get(user, ['uid']).uid
         return self._portal.execute('', 'getApiKeys', param)
 
     def add_key(self, user):
+        """
+        Add User Access Key
+
+        :param str user: Create Access Key for the User
+        """
         param = self._portal.users.get(user, ['uid']).uid
         logging.getLogger().info('Creating s3 API Key for user. %s', {'user': str(user)})
         response = self._portal.execute('', 'createApiKey', param)
@@ -19,6 +31,12 @@ class S3Keys(BaseCommand):
         return response
 
     def delete_key(self, user, access_key):
+        """
+        Delete User Access Key
+
+        :param str user: Retrieve Access Key uid for the User
+        :param str access_key: Delete the Access Key
+        """
         for current_key in self.get_keys(user):
             if access_key == current_key.accessKey:
                 logging.getLogger().info('Deleting s3 API Key. %s', {'access_key': access_key})
