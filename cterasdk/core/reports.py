4df4a5927d36d0f540c7df9bfc3d0f74018eba87
Saimon Michelson <saimon@localhost.localdomain>
2024-04-03 20:31:04 -0400

    update reports to pass lint and ut

--- cterasdk/core/reports.py
+++ cterasdk/core/reports.py
@@ -13,38 +13,38 @@ class Reports(BaseCommand):
         Retrieve the portals statistics report.\n
         To retrieve this report, you must first browse the Global Administration Portal, using: `GlobalAdmin.portals.browse_global_admin()`
         """
-        return self._get_report(Reports.Storage)
+        return self._get_report(enum.Reports.Storage)
 
     def portals(self):
         """
         Retrieve the storage statistics report.\n
         To retrieve this report, you must first browse the Global Administration Portal, using: `GlobalAdmin.portals.browse_global_admin()`
         """
-        return self._get_report(Reports.Portals)
+        return self._get_report(enum.Reports.Portals)
 
     def folders(self):
         """
         Retrieve the cloud folders statistics report.\n
         To retrieve this report, you must first browse the Virtual Portal that contains the report, using: `GlobalAdmin.portals.browse()`
         """
-        return self._get_report(Reports.Folders)
+        return self._get_report(enum.Reports.Folders)
 
     def folder_groups(self):
         """
         Retrieve the folder groups statistics report.\n
         To retrieve this report, you must first browse the Virtual Portal that contains the report, using: `GlobalAdmin.portals.browse()`
         """
-        return self._get_report(Reports.FolderGroups)
-    
+        return self._get_report(enum.Reports.FolderGroups)
+
     def generate(self, name):
         """
         Generate a CTERA Portal Report
 
         :param cterasdk.core.enum.reports report: Report
         """
-        options = {v: k for k,v in enum.Reports.__dict__.items() if not k.startswith('_')}
+        options = {v: k for k, v in enum.Reports.__dict__.items() if not k.startswith('_')}
         if options.get(name, None) is None:
-            raise InputError('Invalid report type.', name, list(options.values()))
+            raise InputError('Invalid report type', name, list(options.values()))
         logging.getLogger().info('Running Portal Report. %s', {'name': name})
         self._core.api.execute('', 'generateReport', name)
 
