c93ebe2e896d24bcee40f7c8a0ddf79fc1f002d0
Saimon Michelson <saimon.michelson@gmail.com>
2024-05-04 16:07:07 -0400

    add storage classes tests

--- tests/ut/test_core_storage_classes.py
+++ tests/ut/test_core_storage_classes.py
@@ -10,6 +10,7 @@ class TestCoreStorageClasses(base_core.BaseCoreTest):
     def setUp(self):
         super().setUp()
         self._storage_class_name = 'Test'
+        self._mock_session = self.patch_call("cterasdk.objects.services.Management.session")
 
     def test_add_storage_class(self):
         add_response = 'Success'
@@ -20,3 +21,12 @@ class TestCoreStorageClasses(base_core.BaseCoreTest):
         expected_param = munch.Munch(dict(name=self._storage_class_name))
         self._assert_equal_objects(actual_param, expected_param)
         self.assertEqual(ret, add_response)
+
+    def test_get_storage_class_global_admin(self):
+        self._mock_session.in_tenant_context = mock.MagicMock()
+        self._mock_session.in_tenant_context.return_value = True
+        get_response = 'Success'
+        self._init_global_admin(get_response=get_response)
+        ret = storage_classes.StorageClasses(self._global_admin).get(self._storage_class_name)
+        self._global_admin.api.get.assert_called_once_with(f'/storageClasses/{self._storage_class_name}')
+        self.assertEqual(ret, get_response)
