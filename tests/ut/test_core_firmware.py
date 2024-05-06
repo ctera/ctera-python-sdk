97ce4313ba1de403a9d2881c4de413b24bcecb5c
Saimon Michelson <saimon.michelson@gmail.com>
2024-05-06 15:36:15 -0400

    add tests for firmwares

---
+++ tests/ut/test_core_firmware.py
@@ -0,0 +1,27 @@
+from unittest import mock
+
+import munch
+from cterasdk.core import firmwares
+from tests.ut import base_core
+
+
+class TestCoreFirmwares(base_core.BaseCoreTest):
+
+    def test_list_images_in_tenant_context(self):
+        self._enable_tenant_context()
+        execute_response = 'Success'
+        self._init_global_admin(execute_response=execute_response)
+        ret = firmwares.Firmwares(self._global_admin).list_images()
+        self._global_admin.api.execute.assert_called_once_with('', 'getFirmwares', mock.ANY)
+        expected_param = munch.Munch({'_classname': 'FirmwareParam'})
+        actual_param = self._global_admin.api.execute.call_args[0][2]
+        self._assert_equal_objects(actual_param, expected_param)
+        self.assertEqual(ret, execute_response)
+
+    def test_list_images_global_admin(self):
+        self._disable_tenant_context()
+        get_response = 'Success'
+        self._init_global_admin(get_response=get_response)
+        ret = firmwares.Firmwares(self._global_admin).list_images()
+        self._global_admin.api.get.assert_called_once_with('/firmwares')
+        self.assertEqual(ret, get_response)
