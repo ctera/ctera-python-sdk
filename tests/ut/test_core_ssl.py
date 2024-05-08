dda1d2c6cbd7f69c318ca0891eddb57a35d6b659
Saimon Michelson <saimon.michelson@gmail.com>
2024-05-08 14:27:43 -0400

    add core ssl tests

---
+++ tests/ut/test_core_ssl.py
@@ -0,0 +1,26 @@
+from unittest import mock
+
+import munch
+from cterasdk.core import ssl
+from tests.ut import base_core
+
+
+class TestCoreSSL(base_core.BaseCoreTest):
+
+    def setUp(self):
+        super().setUp()
+
+    def test_get_certificate(self):
+        get_response = 'Success'
+        self._init_global_admin(get_response=get_response)
+        ret = ssl.SSL(self._global_admin).get()
+        self._global_admin.api.get.assert_called_once_with('/settings/ca')
+        self.assertEqual(ret, get_response)
+
+    def test_thumbprint(self):
+        thumbprint = 'thumbprint'
+        get_response = munch.Munch({'thumbprint': thumbprint})
+        self._init_global_admin(get_response=get_response)
+        ret = ssl.SSL(self._global_admin).thumbprint
+        self._global_admin.api.get.assert_called_once_with('/settings/ca')
+        self.assertEqual(ret, thumbprint)
