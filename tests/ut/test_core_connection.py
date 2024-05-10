ba31983a7c356c0a03ff3a6f88ad0bc7ea415fcc
Saimon Michelson <58434973+saimonation@users.noreply.github.com>
2024-05-10 18:52:47 -0400

    Saimon/add unittests (#232)

---
+++ tests/ut/test_core_connection.py
@@ -0,0 +1,16 @@
+import munch
+from cterasdk.core import connection
+from tests.ut import base_core
+
+
+class TestCoreConnection(base_core.BaseCoreTest):
+
+    def test_run_cli_command(self):
+        name = 'name'
+        version = 'version'
+        get_response = munch.Munch({'version': version, 'name': name})
+        self.patch_call('cterasdk.core.connection.tcp_connect')
+        self._init_setup(get_response=get_response)
+        ret = connection.test(self._global_admin)
+        self._global_admin.ctera.get.assert_called_once_with('/public/publicInfo')
+        self.assertEqual(ret.name, name)
