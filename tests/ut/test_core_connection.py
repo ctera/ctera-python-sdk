774cf7bb560a2d5ac50db0399baa3cee8202b53b
Saimon Michelson <saimon.michelson@gmail.com>
2024-05-10 16:48:13 -0400

    update conn test

--- tests/ut/test_core_connection.py
+++ tests/ut/test_core_connection.py
@@ -11,6 +11,6 @@ class TestCoreConnection(base_core.BaseCoreTest):
         get_response = munch.Munch({'version': version, 'name': name})
         self._init_setup(get_response=get_response)
         self.patch_call('cterasdk.common.utils.tcp_connect')
-        ret = connection.Connection(self._global_admin).test()
+        ret = connection.test(self._global_admin)
         self._global_admin.ctera.get.assert_called_once_with('/public/publicInfo')
         self.assertEqual(ret.name, name)
