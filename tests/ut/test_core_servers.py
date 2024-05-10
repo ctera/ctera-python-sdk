1bb69e0aabb0b056a6d68920370f09e83a812d40
Saimon Michelson <saimon.michelson@gmail.com>
2024-05-10 16:31:27 -0400

    update error message

--- tests/ut/test_core_servers.py
+++ tests/ut/test_core_servers.py
@@ -83,7 +83,7 @@ class TestCoreServers(base_core.BaseCoreTest):
         with self.assertRaises(exceptions.CTERAException) as error:
             servers.Servers(self._global_admin).modify(self._server)
         self._global_admin.api.get.assert_called_once_with(f'/servers/{self._server}')
-        self.assertEqual('Failed to retrieve servers', error.exception.message)
+        self.assertEqual('Failed to retrieve server', error.exception.message)
 
     def test_modify_server_update_failure(self):
         self._init_global_admin(get_response=self._server)
