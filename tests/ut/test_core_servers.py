6fc0cab0b78621fe09ab8c11d33e9fa5e74800b3
Saimon Michelson <saimon.michelson@gmail.com>
2024-05-10 16:28:24 -0400

    update core servers ut

--- tests/ut/test_core_servers.py
+++ tests/ut/test_core_servers.py
@@ -53,10 +53,10 @@ class TestCoreServers(base_core.BaseCoreTest):
         self._global_admin.api.get.assert_called_once_with(f'/servers/{self._server}/schedTasks')
 
     def test_get_servers_success(self):
-        get_multi_response = munch.Munch({'name': self._name})
+        get_multi_response = munch.Munch({'name': self._server})
         self._init_global_admin(get_multi_response=get_multi_response)
         ret = servers.Servers(self._global_admin).get(self._server)
-        self._global_admin.api.get_multi.assert_called_once_with(f'/servers/{self._server}')
+        self._global_admin.api.get_multi.assert_called_once_with(f'/servers/{self._server}', mock.ANY)
         expected_include = ['/' + attr for attr in servers.Servers.default]
         actual_include = self._global_admin.api.get_multi.call_args[0][1]
         self.assertEqual(len(expected_include), len(actual_include))
@@ -69,7 +69,7 @@ class TestCoreServers(base_core.BaseCoreTest):
         self._init_global_admin(get_multi_response=get_multi_response)
         with self.assertRaises(exceptions.ObjectNotFoundException) as error:
             servers.Servers(self._global_admin).get(self._server)
-        self._global_admin.api.get_multi.assert_called_once_with(f'/servers/{self._server}')
+        self._global_admin.api.get_multi.assert_called_once_with(f'/servers/{self._server}', mock.ANY)
         expected_include = ['/' + attr for attr in servers.Servers.default]
         actual_include = self._global_admin.api.get_multi.call_args[0][1]
         self.assertEqual(len(expected_include), len(actual_include))
@@ -78,6 +78,7 @@ class TestCoreServers(base_core.BaseCoreTest):
         self.assertEqual('Could not find server', error.exception.message)
 
     def test_modify_server_not_found(self):
+        self._init_global_admin()
         with self.assertRaises(exceptions.CTERAException) as error:
             servers.Servers(self._global_admin).modify(self._server)
         self._global_admin.api.get.assert_called_once_with(f'/servers/{self._server}')
@@ -85,6 +86,7 @@ class TestCoreServers(base_core.BaseCoreTest):
 
     def test_modify_server_update_failure(self):
         self._init_global_admin(get_response=self._server)
+        self._global_admin.put = mock.MagicMock(side_effect=exceptions.CTERAException())
         with self.assertRaises(exceptions.CTERAException) as error:
             servers.Servers(self._global_admin).modify(self._server)
         self._global_admin.api.get.assert_called_once_with(f'/servers/{self._server}')
