d47b65302cdee4d2112012a54f16a4fe5a785404
Saimon Michelson <saimon.michelson@gmail.com>
2024-06-25 15:12:18 -0400

    update decompression funcs

--- cterasdk/direct/decompressor.py
+++ cterasdk/direct/decompressor.py
@@ -13,6 +13,43 @@ def decompress(block) -> bytes:
     return __decompress_snappy_with_magic(block) if block[1:7] == b'SNAPPY' else __decompress_snappy_without_magic(block)
 
 
+
+def _chunks(compressed_block):
+    """
+    Get Chunks Range if Compression Performed Using Chunks.
+
+    :param bytes block: Compressed Block
+    :returns: List of Slices
+    :rtype: list[(int, int)]
+    """
+    chunks = []
+    size_of_data = len(compressed_block)
+    chunk_size, chunk_start = 4, 16, None
+    while chunk_start < size_of_data:
+        chunk_end = chunk_start + chunk_size + int.from_bytes(compressed_block[chunk_start:chunk_start + chunk_size])
+        chunk_start = chunk_start + chunk_size
+        if chunk_end > size_of_data:
+            break
+        chunks.append((chunk_start, chunk_end))
+        chunk_start = chunk_end
+    return chunks
+
+
+def _decompress_chunks(compressed_block):
+    """
+    Decompress a Block.
+
+    :param bytes block: Compressed Block
+    :returns: Decompressed Block
+    :rtype: bytes
+    """
+    decompressed_block = bytes()
+    chunks = _chunks(compressed_block)
+    for chunk_start, chunk_end in chunks:
+        decompressed_block += snappy.decompress(compressed_block[chunk_start:chunk_end])
+    return decompressed_block
+
+
 def __decompress_snappy_with_magic(block) -> bytes:
     """
     Method to decompress Snappy compressed data with a magic header.
