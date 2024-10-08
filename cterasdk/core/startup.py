c51de1df85ae3825dbcf5e63a25aac7c70172a8a
Saimon Michelson <saimon.michelson@gmail.com>
2024-10-08 16:05:56 -0400

    startup error check

--- cterasdk/core/startup.py
+++ cterasdk/core/startup.py
@@ -16,11 +16,7 @@ class Startup(BaseCommand):
         """
         Get the server startup status
         """
-        response = None
-        try:
-            response = self._core.ctera.get('/startup')
-        except ClientResponseException as error:
-            return error.response.body.status
+        response = self._core.ctera.get('/startup')
         return response.status
 
     def wait(self, retries=120, seconds=5):
@@ -30,17 +26,17 @@ class Startup(BaseCommand):
         attempt = 0
         while True:
             try:
+                if attempt >= retries:
+                    logging.getLogger('cterasdk.core').error('Timed out. Server did not start in a timely manner.')
+                    raise CTERAException('Timed out. Server did not start in a timely manner')
                 current_status = self.status()
                 if current_status == Startup.Started:
                     logging.getLogger('cterasdk.core').info('Server started.')
                     break
                 logging.getLogger('cterasdk.core').debug('Current server status. %s', {'status': current_status})
                 attempt = attempt + 1
-                if attempt >= retries:
-                    logging.getLogger('cterasdk.core').error('Timed out. Server did not start in a timely manner.')
-                    raise CTERAException('Timed out. Server did not start in a timely manner')
                 time.sleep(seconds)
-            except (ConnectionError, TimeoutError) as e:
+            except (ConnectionError, TimeoutError, ClientResponseException) as e:
                 logging.getLogger('cterasdk.core').debug('Exception. %s', e.__dict__)
                 attempt = attempt + 1
                 time.sleep(seconds)
