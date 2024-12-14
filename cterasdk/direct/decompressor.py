3de4c8d66e1d1c57a2401b6dc5a44ad5354a8b72
Saimon Michelson <58434973+saimonation@users.noreply.github.com>
2024-12-14 13:56:24 -0800

    remove CBC padding (#269)

--- cterasdk/direct/decompressor.py
+++ cterasdk/direct/decompressor.py
@@ -11,8 +11,11 @@ def decompress(compressed_block):
     :returns: Decompressed Block
     :rtype: bytes
     """
-    return decompress_with_magic_header(compressed_block) \
-        if compressed_block.startswith(b'\x82SNAPPY\x00') else decompress_without_magic_header(compressed_block)
+    try:
+        return decompress_with_magic_header(compressed_block) \
+            if compressed_block.startswith(b'\x82SNAPPY\x00') else snappy.uncompress(compressed_block)
+    except snappy.UncompressError:
+        raise DirectIOError()
 
 
 def decompress_with_magic_header(compressed_block):
@@ -23,29 +26,15 @@ def decompress_with_magic_header(compressed_block):
     :returns: Decompressed Block
     :rtype: bytes
     """
-    try:
-        logging.getLogger('cterasdk.direct').debug('Decompressing Block.')
-        decompressed_block = bytearray()
-        size_of_data = len(compressed_block)
-        chunk_size, chunk_start = 4, 16
-        while chunk_start < size_of_data:
-            chunk_end = chunk_start + chunk_size + int.from_bytes(compressed_block[chunk_start:chunk_start + chunk_size])
-            chunk_start = chunk_start + chunk_size
-            if chunk_end > size_of_data:
-                break
-            decompressed_block.extend(snappy.decompress(compressed_block[chunk_start:chunk_end]))
-            chunk_start = chunk_end
-        return decompressed_block
-    except snappy.UncompressError:
-        raise DirectIOError()
-
-
-def decompress_without_magic_header(compressed_block):
-    """
-    Decompress a Block.
-
-    :param bytes compressed_block: Compressed Block
-    :returns: Decompressed Block
-    :rtype: bytes
-    """
-    return snappy.uncompress(compressed_block[:compressed_block.rfind(b'EOF') + 3])
+    logging.getLogger('cterasdk.direct').debug('Decompressing Block.')
+    decompressed_block = bytearray()
+    size_of_data = len(compressed_block)
+    chunk_size, chunk_start = 4, 16
+    while chunk_start < size_of_data:
+        chunk_end = chunk_start + chunk_size + int.from_bytes(compressed_block[chunk_start:chunk_start + chunk_size])
+        chunk_start = chunk_start + chunk_size
+        if chunk_end > size_of_data:
+            break
+        decompressed_block.extend(snappy.decompress(compressed_block[chunk_start:chunk_end]))
+        chunk_start = chunk_end
+    return decompressed_block
