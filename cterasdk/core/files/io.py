2ec44298ca37444e9465ccbff526743edaee4216
Saimon Michelson <saimon.michelson@gmail.com>
2025-05-01 10:31:27 -0400

    update to pass pylint

--- cterasdk/core/files/io.py
+++ cterasdk/core/files/io.py
@@ -109,7 +109,7 @@ def handle(path):
 
 def handle_many(directory, *objects):
     """
-    Create function to retrieve zip archive 
+    Create function to retrieve zip archive
 
     :param cterasdk.cio.edge.CorePath directory: Path to directory.
     :param *str objects: List of files and folders.
--- cterasdk/edge/files/io.py
+++ cterasdk/edge/files/io.py
@@ -65,7 +65,7 @@ def handle(path):
 
 def handle_many(directory, *objects):
     """
-    Create function to retrieve zip archive 
+    Create function to retrieve zip archive
 
     :param cterasdk.cio.edge.EdgePath directory: Path to directory.
     :param *str objects: List of files and folders.
