6fbfe3bf01c0e81340963cf886509b98363fbc38
Saimon Michelson <saimon.michelson@gmail.com>
2025-05-18 19:12:35 -0400

    resolve circular import

--- cterasdk/settings.py
+++ cterasdk/settings.py
@@ -3,7 +3,6 @@ import logging
 from pathlib import Path
 import yaml
 from .convert import fromjsonstr
-from .lib.storage import commonfs
 
 
 logger = logging.getLogger('cterasdk')
@@ -11,14 +10,9 @@ logger = logging.getLogger('cterasdk')
 
 settings = Path(__file__).parent.absolute().joinpath('settings.yml')
 try:
-    commonfs.properties(settings)
+    with open(settings, 'r', encoding='utf-8') as f:
+        settings = fromjsonstr(json.dumps(yaml.safe_load(f)))
+    core, edge, drive, io, audit = settings.core, settings.edge, settings.drive, settings.io, settings.audit
 except FileNotFoundError:
     logger.fatal("Configuration file 'settings.yml' not found. Please check your installation and try again.")
     raise
-
-
-with open(settings, 'r', encoding='utf-8') as f:
-    settings = fromjsonstr(json.dumps(yaml.safe_load(f)))
-
-
-core, edge, drive, io, audit = settings.core, settings.edge, settings.drive, settings.io, settings.audit
