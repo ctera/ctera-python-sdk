95bbebf48a09f9aa49b522043dfe01e5164822b7
Saimon Michelson <saimon.michelson@gmail.com>
2026-01-03 19:21:58 -0500

    update test versions

--- tests/ut/core/user/test_versions.py
+++ tests/ut/core/user/test_versions.py
@@ -1,3 +1,5 @@
+import munch
+from datetime import datetime
 from tests.ut.core.user import base_user
 
 
@@ -5,8 +7,16 @@ class BaseCoreServicesFilesVersions(base_user.BaseCoreServicesTest):
 
     def test_list_versions(self):
         directory = 'My Files'
-        execute_response = 'Success'
-        self._init_services(execute_response=execute_response)
+        self._init_services(execute_response=[self._create_snapshot_response(directory, True)])
         ret = self._services.files.versions(directory)
         self._services.api.execute.assert_called_once_with('', 'listSnapshots', f'{self._base}/{directory}')
-        self.assertEqual(ret, execute_response)
+        self.assertEqual(str(ret.path), directory)
+
+    def _create_snapshot_response(self, path, current):
+        return munch.Munch({
+            'url': self._base,
+            'path': path,
+            'current': current,
+            'startTimestamp': datetime.now().isoformat(),
+            'calculatedTimestamp': datetime.now().isoformat()
+        })
