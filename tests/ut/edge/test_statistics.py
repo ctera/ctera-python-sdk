69d2616789ff14e6e4f2c82515ee85fa00b5c065
Saimon Michelson <saimon.michelson@gmail.com>
2026-05-19 19:25:48 -0400

    update tests

--- tests/ut/edge/test_statistics.py
+++ tests/ut/edge/test_statistics.py
@@ -10,49 +10,50 @@ class TestEdgeStatistics(base_edge.BaseEdgeTest):
         self.intervals = (Interval.Hour, Interval.Day, Interval.Week, Interval.Month, Interval.Year)
 
     def test_cpu(self):
-        self._init_filer()
         for interval in self.intervals:
-            statistics.Statistics(self._filer).statistics.cpu(interval)
+            self._init_filer()
+            statistics.Statistics(self._filer).cpu(interval)
             self._filer.api.get.assert_called_with(Metric.CPU, params={'interval': interval})
 
     def test_memory(self):
         self._init_filer()
         for interval in self.intervals:
-            statistics.Statistics(self._filer).statistics.memory(interval)
+            self._init_filer()
+            statistics.Statistics(self._filer).memory(interval)
             self._filer.api.get.assert_called_with(Metric.Memory, params={'interval': interval})
 
     def test_volume(self):
-        self._init_filer()
         for interval in self.intervals:
-            statistics.Statistics(self._filer).statistics.volume(interval)
+            self._init_filer()
+            statistics.Statistics(self._filer).volume(interval)
             self._filer.api.get.assert_called_with(Metric.Volume, params={'interval': interval})
 
     def test_cache(self):
-        self._init_filer()
         for interval in self.intervals:
-            statistics.Statistics(self._filer).statistics.cache(interval)
+            self._init_filer()
+            statistics.Statistics(self._filer).cache(interval)
             self._filer.api.get.assert_called_with(Metric.Cache, params={'interval': interval})
 
     def test_connections(self):
-        self._init_filer()
         for interval in self.intervals:
-            statistics.Statistics(self._filer).statistics.connections(interval)
+            self._init_filer()
+            statistics.Statistics(self._filer).connections(interval)
             self._filer.api.get.assert_called_with(Metric.Connections, params={'interval': interval})
 
     def test_disk(self):
-        self._init_filer()
         for interval in self.intervals:
-            statistics.Statistics(self._filer).statistics.disk(interval)
+            self._init_filer()
+            statistics.Statistics(self._filer).disk(interval)
             self._filer.api.get.assert_called_with(Metric.Disk, params={'interval': interval})
 
     def test_local_io(self):
-        self._init_filer()
         for interval in self.intervals:
-            statistics.Statistics(self._filer).statistics.local_io(interval)
+            self._init_filer()
+            statistics.Statistics(self._filer).local_io(interval)
             self._filer.api.get.assert_called_with(Metric.Local, params={'interval': interval})
 
     def test_cloudsync(self):
-        self._init_filer()
         for interval in self.intervals:
-            statistics.Statistics(self._filer).statistics.cloudsync(interval)
+            self._init_filer()
+            statistics.Statistics(self._filer).cloudsync(interval)
             self._filer.api.get.assert_called_with(Metric.CloudSync, params={'interval': interval})
